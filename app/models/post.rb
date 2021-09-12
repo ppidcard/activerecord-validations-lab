aa502cb36d0649ec87169fae868af1e7ba5b92bf
ppidcard <tvxqlikei@live.cn>
2021-09-12 11:09:01 +1000

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates(:content, { :length => { :minimum => 250 } })
+    validates(:summary, { :length => { :maximum => 250 } })
+    validates :category, inclusion: {in:["Fiction", "Non-Fiction"]}
+    validate :clickbait
+
+    def clickbait
+    arr = ["Won't Believe", "Top[number]", "Secret", "Guess"]
+        if title && arr.none? {|f| title.include?(f)}
+            errors.add(:title, "is not clickbait")
+        end
+    end
 end
